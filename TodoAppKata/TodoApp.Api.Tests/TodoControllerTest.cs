77a064e52009aacea76057b65092cdf4d571324f
Maxime PREUILH <maxime.preuilh@external.floa.com>
2023-05-05 16:32:39 +0200

    debut test

---
+++ TodoAppKata/TodoApp.Api.Tests/TodoControllerTest.cs
@@ -0,0 +1,13 @@
+using Xunit;
+using Xunit.Assert;
+
+namespace TodoApp.Api.Tests;
+
+public class TodoControllerTest
+{
+    [Fact]
+    public void WhenNoData_ShouldReturnEmptyOrNull()
+    {
+        
+    }
+}
---
+++ TodoAppKata/TodoApp.Api/Controllers/TodoController.cs
@@ -0,0 +1,35 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace TodoApp.Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class TodoController : ControllerBase
+{
+
+    private readonly ILogger<TodoController> _logger;
+
+    public TodoController(ILogger<TodoController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet(Name = "GetTodo")]
+    public IEnumerable<WeatherForecast> Get([FromQuery] TodoQueryParam todoQueryParam)
+    {
+        return Enumerable
+            .Range(1, 5)
+            .Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55)
+            })
+            .ToArray();
+    }
+}
+
+public class TodoQueryParam
+{
+
+    string? Status;
+}
